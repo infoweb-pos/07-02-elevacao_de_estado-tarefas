0ed19dd734bfa20df738489d296cc1403ebc1bbd
Leonardo Minora <leonardo.minora@gmail.com>
2023-06-15 10:57:36 -0300

    Adicionado componente AppTarefaLista

--- src/App.tsx
+++ src/App.tsx
@@ -18,11 +18,37 @@ const AppTarefaEditar = () => {
 	);
 };
 
+const AppTarefaLista = () => {
+	return (
+		<div className="card">
+			<ul>
+				<li>Prototipar interface do usuário</li>
+				<li>Implementar com HTML a interface com o usário em React</li>
+				<li>Extrair componentes React da implementação HTML</li>
+				<li>
+					Transferir os dados do HTML dos componentes React para
+					variáveis
+				</li>
+				<li>
+					Modificar os dados de variáveis para estado ou propriedades
+					de componentes
+				</li>
+				<li>
+					Elevar os estados dos componentes quando tiver dados
+					compartilhados
+				</li>
+				<li>Programar a modificação dos estados</li>
+			</ul>
+		</div>
+	);
+};
+
 const App = () => {
 	return (
 		<>
 			<AppNavBar />
 			<AppTarefaEditar />
+			<AppTarefaLista />
 		</>
 	);
 };
